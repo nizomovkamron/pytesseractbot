9083da9b7d89461fa9b6d1cfadd9d9eada804578
Kamron <47291727+nizomovkamron@users.noreply.github.com>
2021-04-25 13:21:40 +0500

    Update rec.py
    Added more languages, used cv2. Improved recognition of text in photo

--- rec.py
+++ rec.py
@@ -1,5 +1,5 @@
 
-from PIL import Image
+from PIL import Image,ImageEnhance, ImageFilter
  
 import pytesseract
 import telebot
@@ -9,6 +9,7 @@ import locale
 import db
 import datetime
 import time
+import cv2
 bot = telebot.TeleBot("1700243571:AAFDOlbgHAacwZiU3HJQPHtKDR641niRpnM")
 #print(pytesseract.image_to_string(Image.open('txt.png')).encode('latin-1', 'replace'))
 t = time.localtime()
@@ -31,16 +32,26 @@ def foo(message):
 
 		downloaded_file = bot.download_file(file_info.file_path)
 		rn=file_info.file_path
+		
 
 		with open(rn, 'wb') as new_file:
+
 			new_file.write(downloaded_file)
+			# im = Image.open(rn)
+			# im = im.filter(ImageFilter.MedianFilter())
+			# enhancer = ImageEnhance.Contrast(im)
+			# im = enhancer.enhance(2)
+			# im = im.convert('1')
+			# im.save(rn)
 			
-			
+			config_custom=r'--oem 3 --psm 6'
+			img=cv2.imread(rn)
+			img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 			try:
-				bot.send_message(message.chat.id,pytesseract.image_to_string(Image.open(rn)))
+				bot.send_message(message.chat.id,pytesseract.image_to_string(img,lang="eng+ara+uzb+rus",config=config_custom))
 				db.ins(message.chat.id,int(message.date)+5)
 			except Exception:
-				bot.send_message(message.chat.id,'Расмда ҳеч нима топа олмадим!')
+				bot.send_message(message.chat.id,'Расмда ҳеч нима топа олмадим!\nКўп учрайдиган хатолар:\n* Расмнинг хиралиги\n* Расмнинг ўлчами катталиги\n* Расмдаги матннинг шрифти тўғри келмаётгани')
 			
 		try:
 			os.remove(rn)
